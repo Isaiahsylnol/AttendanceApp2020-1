68c4622e919b8ad91d2320de2899159bfc973b79
Isaiah <40073397+Isaiahsylnol@users.noreply.github.com>
2020-02-21 16:47:38 -0500

    Attendee Model getter & setters

---
+++ AttendanceApplication/Models/AttendanceRegisters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceApplication.Models
+{
+    public class AttendanceRegisters
+    {
+        [Display(Name = "Register ID")]
+        [Required(ErrorMessage = "ID is required.")]
+        public int registerId { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "The registry name is required.")]
+        public string name { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage ="The description is required.")]
+        public string description { get; set; }
+
+        [Display(Name = "Scheme ID")]
+        [Required(ErrorMessage = "The attendance scheme ID is required.")]
+        public int attSchemeId { get; set; }
+    }
+}
